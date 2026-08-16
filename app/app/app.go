e847035953cf04b1ffff760b7e9b501844b9f7b0
ShashankRaoCoding <shashank.jrao@outlook.com>
2026-08-16 21:13:22 +0000

    Changes # Please enter the commit message for your changes. Lines starting

---
+++ app/app/app.go
@@ -0,0 +1,68 @@
+package app
+
+import (
+	// bubble tea related 
+	// tab
+	// msgs 
+)
+
+type App struct {
+	tabs []tab.Tab
+	focus int // the tab index that is focussed 
+	clis []map[string]string
+}
+
+func (a App) Init() {
+	// reads CLIs/* and adds the CLIs 
+	// starts tab 'launcher' 
+}
+
+func (a App) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	// if reserved, interpret
+	// else forward to focus tab with
+	// return a, a.tabs[a.focus].Update(tea.Msg) 
+	// or better equivalent 
+}
+
+
+func (a App) View() string {
+	tabs := a.RenderTabs() 
+	focus := a.tabs[a.focus].View() 
+	return strings.Join([]string{tabs, focus}, "\n") 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
