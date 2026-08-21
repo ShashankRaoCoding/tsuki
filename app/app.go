15aca1d40ec7878ad031800d645ae9771a91fab0
ShashankRaoCoding <shashank.jrao@outlook.com>
2026-08-21 00:35:52 +0000

    Changes

--- app/app.go
+++ app/app.go
@@ -1,12 +1,26 @@
 package app 
 
+import (
+	msgs "github.com/ShashankRaoCoding/tsuki/msgs"
+	tea "github.com/charmbracelet/bubbletea"
 
+)
 
+func (a App) Init() tea.Msg {
+	return nil 
+}
 
+func (a App) Update(m tea.Msg) (tea.Model, tea.Cmd) {
+	var f msgs.MsgFunc
+	var o bool
+	var c tea.Cmd 
 
+	return a, c
+}
 
-
-
+func (a App) View() string {
+	return a.Render() 
+}
 
 
 
