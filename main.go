5e98e9d4d87fa43ef583ff1c35fd8888960e1810
ShashankRaoCoding <shashank.jrao@outlook.com>
2026-08-21 00:28:37 +0000

    Changes

--- main.go
+++ main.go
@@ -8,7 +8,17 @@ import (
 )
 
 func main() {
-	
+	_, err := tea.NewProgram(
+		app.New(),
+		tea.WithAltScreen, 
+	)
+
+	if err == nil {
+		os.Exit(0) 
+	} else {
+		fmt.Printf(err)
+		os.Exit(1) 
+	}
 }
 
 
