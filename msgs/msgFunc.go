705b54a6f373b8829238458df08b0386d5386c19
ShashankRaoCoding <shashank.jrao@outlook.com>
2026-08-21 00:46:33 +0000

    Changes

--- msgs/msgFunc.go
+++ msgs/msgFunc.go
@@ -2,14 +2,14 @@ package msgs
 
 import (
 	"fmt"
-	msgFuncs "github.com/ShashankRaoCoding"
+	msgFuncs "github.com/ShashankRaoCoding/tsuki/msgs/msgFuncs"
 	tea "github.com/charmbracelet/tea"
 )
 
 type MsgFunc func(tea.Model, tea.Msg) (tea.Model, tea.Cmd) 
 
 var Msg2Func = map[string]MsgFunc{
-	
+	"Err": msgFuncs.ErrFunc, 
 }
 
 
