41cb36e9066f4fea0cd0b9ecea12e120f96825e6
ShashankRaoCoding <shashank.jrao@outlook.com>
2026-08-21 19:46:05 +0000

    Changes

--- app/app.go
+++ app/app.go
@@ -4,7 +4,7 @@ import (
 	msgs "github.com/ShashankRaoCoding/tsuki/msgs"
 	tea "github.com/charmbracelet/bubbletea"
 	lipgloss "github.com/charmbracelet/lipgloss"
-
+	shared "github.com/ShashankRaoCoding/tsuki/shared"
 )
 
 func (a App) Init() tea.Msg {
