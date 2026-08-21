86dd7c6e52cd48049ae7c1e8766815592f18fd01
ShashankRaoCoding <shashank.jrao@outlook.com>
2026-08-21 19:24:39 +0000

    Changes

--- main.go
+++ main.go
@@ -1,24 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"os"
-	"fmt" 
-	"github.com/ShashankRaoCoding/tuski/app" 
+	"github.com/ShashankRaoCoding/tsuki/app"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	_, err := tea.NewProgram(
+	var err error
+	_, err = tea.NewProgram(
 		app.New(),
 		tea.WithAltScreen, 
 	)
 
-	if err == nil {
-		os.Exit(0) 
-	} else {
-		fmt.Printf(err)
-		os.Exit(1) 
-	}
+	fmt.Printf(err)
+	os.Exit(0) 
 }
 
 
