2c47e0fd99c750da8c9b7f19f1ff4eeb33427476
ShashankRaoCoding <shashank.jrao@outlook.com>
2026-08-22 13:55:36 +0000

    Changes

--- shared/utils/readJSON.go
+++ shared/utils/readJSON.go
@@ -7,7 +7,7 @@ import (
 	"encoding/json" 
 )
 
-func ReadJSONToStruct(filepath string, s *any) (any, err ) {
+func ReadJSONToStruct(filepath string, s *any) err {
 	var file os.File
 	var fileData []byte 
 	var err error 
