9db069f504c1e4ca31110eb5df8684bf71067e96
ShashankRaoCoding <shashank.jrao@outlook.com>
2026-08-22 13:16:02 +0000

    Changes

--- shared/appConfig.go
+++ shared/appConfig.go
@@ -4,19 +4,30 @@ import (
 	"fmt" 
 	"json/Encoding" 
 	"os" 
+	"io" 
 	"io/fs" 
 )
 
 type AppConfig map[string]string
 
-func Load() AppConfig {
+func LoadApps() ([]AppConfig, error) {
 	var files []fs.DirEntry
 	var err error
+	var apps []AppConfig 
 
 	files, err = os.ReadDir(AppsDir) // from config.go 
 	if err != nil {
-		fmt.Printf("There was an error: Could not Read %s", AppsDir)
-		os.Exit(1) 
+		return apps, fmt.Errorf("There was an error: Could not Read %s", AppsDir)
+	}
+
+	for _, file := range files {
+		var data []byte
+		data, err = io.ReadAll(file) 
+		if err == nil {
+			
+		} else {
+			
+		}
 	}
 }
 
