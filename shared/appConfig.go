767db7973862f12f5645024d9a65353cf7577af6
ShashankRaoCoding <shashank.jrao@outlook.com>
2026-08-22 13:34:16 +0000

    Changes

--- shared/appConfig.go
+++ shared/appConfig.go
@@ -17,7 +17,7 @@ func LoadApps() ([]AppConfig, error) {
 	var apps []AppConfig 
 	var errs []error 
 
-	files, err = os.ReadDir(AppsDir) // from config.go 
+	files, err = os.ReadDir(appsDir) // from config.go 
 	if err != nil {
 		return apps, fmt.Errorf("There was an error: Could not Read %s", AppsDir)
 	}
