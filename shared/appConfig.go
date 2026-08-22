b037dcc3298c19cdf36633b804ea2727a6849158
ShashankRaoCoding <shashank.jrao@outlook.com>
2026-08-22 13:31:22 +0000

    Changes

--- shared/appConfig.go
+++ shared/appConfig.go
@@ -32,10 +32,7 @@ func LoadApps() ([]AppConfig, error) {
 
 		file, err = os.Open(filePath) 
 		if err != nil {
-			errs = append(
-				errs,
-				err , 
-			)
+			errs = append(errs, err) 
 		}
 		data, err = io.ReadAll(file) 
 		if err == nil {
