53cc47a88b3852061f78a84b9af9cfc25aeffd62
ShashankRaoCoding <shashank.jrao@outlook.com>
2026-08-22 23:54:17 +0000

    Changes

--- shared/config/config.go
+++ shared/config/config.go
@@ -1,9 +1,24 @@
 package config
 
+import (
+	"github.com/ShashankRaoCoding/tsuki/shared/utils"
+)
+
+type Config struct {
+	AppConfigDir string 
+	
+}
+
 var ConfigPath = "config.json"
+var CONFIG Config 
 
 func init() {
-	
+	config, err := utils.ReadJSONToStruct(
+		ConfigPath,
+		func() Config {
+			return Config{} 
+		}, 
+	)
 }
 
 
