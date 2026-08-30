42b6702cb1c90deb85d985a529ae810940209e33
ShashankRaoCoding <shashank.jrao@outlook.com>
2026-08-30 20:15:37 +0000

    Changes

--- app/app.go
+++ app/app.go
@@ -1,68 +1,18 @@
-package app 
+package msgs
 
 import (
-	msgs "github.com/ShashankRaoCoding/tsuki/msgs"
+	// "fmt"
+	msgFuncs "github.com/ShashankRaoCoding/tsuki/msgs/msgFuncs"
 	tea "github.com/charmbracelet/bubbletea"
-	lipgloss "github.com/charmbracelet/lipgloss"
-	config "github.com/ShashankRaoCoding/tsuki/shared/config" 
-	appsConfig "github.com/ShashankRaoCoding/tsuki/shared/appsConfig"
-	tabs "github.com/ShashankRaoCoding/tsuki/tabs" 
-	msgFuncs "github.com/ShashankRaoCoding/tsuki/msgs/msgFuncs" 
 )
 
-func New() App {
-	return App{
-		Tabs: []*tabs.Tab{
-			tabs.New(), 
-		}, 
-	} 
-}
+type MsgFunc func(tea.Model, tea.Msg) (tea.Model, tea.Cmd) 
 
-type App struct {
-	Height int 
-	Width int 
-	Tabs []*tab.Tab
-	Apps map[string]appsConfig.AppConfig
-	Focus int 
-}
+var Msg2Func = map[string]MsgFunc{}
 
-func (a App) Init() tea.Cmd {
-	_apps, err = appsConfig.LoadApps(".apps/") 
-	for _, app := range _apps {
-		a.Apps[app.Name] = app
-	}
 
-	return nil 
-}
 
-func (a App) Update(m tea.Msg) (tea.Model, tea.Cmd) {
-	var f msgs.MsgFunc
-	var o bool
-	var c tea.Cmd 
 
-	f, o = msgs.Msg2Func[m]
-	if o == false {
-		f, _ = msgs.Msg2Func["msgFuncs.ErrMsg"] 
-	}
-
-	_a, c = f(a, m) 
-	a, _ = _a.(App) 
-	return a, c
-}
-
-func (a App) View() string {
-	tabsLabels := lipgloss.JoinHorizontal(
-		lipgloss.Left,
-		renderTabsLabels(a) ..., 
-	)
-	return lipgloss.JoinVertical(
-		lipgloss.Top,
-		[]string{
-			tabsLabels,
-			a.Tabs[a.Focus].View(), 
-		}
-	)
-}
 
 
 
