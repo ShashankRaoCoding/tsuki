8215008668c69fc605560759b10a54db8a56b398
ShashankRaoCoding <shashank.jrao@outlook.com>
2026-09-02 00:44:16 +0000

    Changes

--- tsuki/app/app.go
+++ tsuki/app/app.go
@@ -1,14 +1,8 @@
-package msgs
+package app 
 
-import (
-	// "fmt"
-	msgFuncs "github.com/ShashankRaoCoding/tsuki/msgs/msgFuncs"
-	tea "github.com/charmbracelet/bubbletea"
-)
 
-type MsgFunc func(tea.Model, tea.Msg) (tea.Model, tea.Cmd) 
 
-var Msg2Func = map[string]MsgFunc{}
+
 
 
 
--- tsuki/msgs/errMsg.go
+++ tsuki/msgs/errMsg.go
@@ -1,14 +1,14 @@
-package msgFuncs
+package msgs
 
 import (
 	"fmt"
 	tea "github.com/charmbracelet/bubbletea" 
-	msgs "github.com/ShashankRaoCoding/tsuki/msgs"
+	// msgs "githu.com/ShashankRaoCoding/tsuki/msgs"
 	"log" 
 )
 
 func init() {
-	msgs.Msg2Func["msgFuncs.ErrMsg"] = ErrFunc
+	Msg2Func["msgFuncs.ErrMsg"] = ErrFunc
 }
 
 func ErrFunc(a tea.Model, m tea.Msg) (tea.Model, tea.Cmd) {
